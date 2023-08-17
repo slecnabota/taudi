67936d40ca5dc7dc520d93a993361178e0a34fb8
Akbota <akbotamold@gmail.com>
2023-08-17 17:52:17 +0600

    certificate

--- index.js
+++ index.js
@@ -34,3 +34,62 @@ const product = new Swiper('#productSwiper', {
   }
 });
 
+
+// product open Modal Cards
+function initModalSlider() {
+  return new Swiper('#modalSwiper', {
+    direction: 'horizontal',
+    loop: true,
+    navigation: {
+      nextEl: '#modalNext',
+      prevEl: '#modalPrev',
+    },
+  });
+}
+let modalSlider = initModalSlider();
+
+const slides = document.querySelectorAll(".product-slide");
+
+slides.forEach((slide, index) => {
+  slide.addEventListener("click", () => {
+    modalSlider.destroy();
+    modalSlider = initModalSlider();
+    modalSlider.slideTo(index);
+    openModal();
+  });
+});
+
+const closeModalButton = document.getElementById("modalClose");
+closeModalButton.addEventListener("click", closeModal);
+
+function openModal() {
+  const modal = document.getElementById("productModal");
+  modal.style.display = "flex";
+}
+
+function closeModal() {
+  const modal = document.getElementById("productModal");
+  modal.style.display = "none";
+}
+
+const certificate = new Swiper('#certificateSwiper', {
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '#certificateNext',
+    prevEl: '#certificatePrev',
+  },
+  breakpoints: {
+    320: {
+
+    },
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 25,
+    },
+    1601: {
+      slidesPerView: 2,
+      spaceBetween: 100,
+    }
+  }
+});
