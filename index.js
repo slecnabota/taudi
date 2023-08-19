0620e1494fa0daec1d5a209c7dab4e3ebbf57cbe
Akbota <akbotamold@gmail.com>
2023-08-20 03:28:16 +0600

    first commit

--- index.js
+++ index.js
@@ -126,3 +126,147 @@ function openCertificateModal(slideIndex) {
     certificateMSwiper.destroy();
   });
 }
+$(document).ready(function () {
+  $('#city-menu li').click(function () {
+    // Убираем класс selected со всех li
+    $('#city-menu li').removeClass('selected');
+
+    // Добавляем класс selected к выбранному li
+    $(this).addClass('selected');
+
+    var markerId = $(this).data('marker');
+    var marker = markers[markerId];
+
+    if (marker && marker.markerObj) {
+        marker.markerObj.openPopup();
+    }
+});
+});
+
+var markers = {
+  marker1: {
+    address: 'г.Астана',
+    address2:'',
+    phone: 'Телефон 1',
+    coords: [51.169392, 71.449074] 
+  },
+  marker2: {
+    address: 'г.Алматы',
+    address2:'г. Алматы, пр. Сүйiнбай, 314',
+    phone: 'Телефон 2',
+    coords: [43.238949, 76.889709]  
+  },
+  marker3: {
+    address: 'г.Абай',
+    address2:'',
+    phone: 'Телефон 2',
+    coords: [49.631566, 72.853566]
+  },
+  marker4: {
+    address: 'г.Балхаш',
+    address2:'',
+    phone: 'Телефон 1',
+    coords: [46.840499, 74.980003]
+  },
+  marker5: {
+    address: 'Жезказган',
+    address2:'',
+    phone: 'Телефон 2',
+    coords: [47.782892, 67.709953]  
+  },
+  
+  marker6: {
+    address: 'г.Костанай',
+    address2:'',
+    phone: 'Телефон 1',
+    coords: [53.214123, 63.635843]
+  },
+  marker7: {
+    address: 'г.Кокшетау',
+    address2:'',
+    phone: 'Телефон 2',
+    coords: [53.283156, 69.378744]  
+  },
+  marker8: {
+    address: 'г.Караганда',
+    address2:'',
+    phone: 'Телефон 1',
+    coords: [49.802392, 73.096834]  
+  },
+  marker9: {
+    address: 'г.Караганда',
+    address2:'',
+    phone: 'Телефон 2',
+    coords: [49.802392, 73.096834]  
+  },
+
+
+  marker10: {
+    address: 'г.Петропавловск',
+    address2:'',
+    phone: 'Телефон 1',
+    coords: [54.875900, 69.162378] 
+  },
+  marker11: {
+    address: 'г.Сарань',
+    address2:'',
+    phone: 'Телефон 2',
+    coords: [51.187957, 51.371245]   
+  },
+  marker12: {
+    address: 'г.Талдыкорган',
+    address2:'',
+    phone: 'Телефон 1',
+    coords: [45.015850, 78.377134]
+  },
+  marker13: {
+    address: 'г.Темиртау',
+    address2:'',
+    phone: 'Телефон 2',
+    coords:[50.051162, 72.964872]
+  },
+
+
+  marker14: {
+    address: 'г.Шатинск',
+    address2:'',
+    phone: 'Телефон 1',
+    coords: [52.026777, 76.920918]
+  },
+  marker15: {
+    address: 'г.Щучинск',
+    address2:'',
+    phone: 'Телефон 2',
+    coords: [52.933681, 70.199899]  
+  },
+  marker16: {
+    address: 'г.Экибастуз',
+    address2:'',
+    phone: 'Телефон 1',
+    coords: [51.709416, 75.322932]
+  },
+};
+
+var map = L.map('map').setView([48.0196, 66.9237], 5);
+
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+for (var markerId in markers) {
+  var marker = markers[markerId];
+  marker.markerObj = L.marker(marker.coords).addTo(map);
+  marker.markerObj.bindPopup(`<h3>Официальный дистрибьютор по ${marker.address} ТОО «TAUDI»</h3> ` + 
+  '<p>marker.address2</p>' + 
+  `
+  <div class="address-tel">
+    <img src="assets/svg/tel-map.svg">
+    <div class="adress-tel__inner">
+      <a href="tel:+77076622515">+7-707-662-25-15</a>
+      <a href="tel:+77078208880">+7-707-820-88-805</a>
+    </div>
+  </div>
+  
+  `)
+}
